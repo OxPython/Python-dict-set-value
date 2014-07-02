87073f6956be6b0bd5de175e39ab8ed89500cab8
Emerson <viejoemer@gmail.com>
2014-07-02 10:04:35 -0500

    #HowTo create and set a value in a dict in python?

---
+++ dict_set_value.py
@@ -0,0 +1,30 @@
+'''
+Created on Jul 1, 2014
+
+@author: viejoemer
+'''
+############################################################
+#HowTo create and set a value in a dict in python?
+
+#Creating a dict with data
+
+d = {
+     "red":100,
+     "yellow":200,
+     "blue" : 300
+     }
+
+print(d)
+
+#Set a value
+d["red"] = 111
+
+print(d)
+
+#Create a key and set a value
+
+d["black"] = 400
+
+print(d)
+
+print(d)
